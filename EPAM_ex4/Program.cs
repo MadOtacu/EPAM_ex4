a23771e45ba93dbfeea82b0d67cc670b3ebc87f8
ЬфвЩефсг <pipboy1917@gmail.com>
2020-03-18 03:56:33 +0400

    Добавьте файлы проекта.

---
+++ EPAM_ex4/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace EPAM_ex4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = Int32.Parse(Console.ReadLine());
+            Console.WriteLine(Factorial(num));
+        }
+        static int Factorial(int x)
+        {
+            if (x == 0)
+            {
+                return 1;
+            }
+            else
+            {
+                return x * Factorial(--x);
+            }
+        }
+    }
+}
